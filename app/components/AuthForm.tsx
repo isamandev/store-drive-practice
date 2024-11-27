e2b810cb7c077a07d1ce7bc15d269a8bd607f888
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-11-27 11:14:51 +0330

    OTP Modal

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -18,6 +18,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { createAccount } from '@/lib/actions/user.actions';
+import OTPModal from './OTPModal';
 
 type FormType = 'sign-in' | 'sign-up';
 
@@ -132,7 +133,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
 					</div>
 				</form>
 			</Form>
-			{/* OTP Verification */}
+			{accountId && <OTPModal email={form.getValues('email')} accountId={accountId} />}
 		</>
 	);
 };
