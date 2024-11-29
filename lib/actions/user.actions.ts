1fa24e3b9dd65eb451427d2081b47d81af306d07
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-11-29 17:07:02 +0330

    Layout, Nav, Sidebar

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
 import { ID, Query } from 'node-appwrite';
-import { createAdminClient } from '../appwrite';
+import { createAdminClient, createSessionClient } from '../appwrite';
 import { appwriteConfig } from '../appwrite/config';
 import { parseStringify } from '../utils';
 import { cookies } from 'next/headers';
+import { avatarPlaceholderUrl } from '@/constants';
 
 const getUserByEmail = async (email: string) => {
 	const { databases } = await createAdminClient();
@@ -53,8 +54,7 @@ export const createAccount = async ({
 			{
 				fullName,
 				email,
-				avatar:
-					'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541',
+				avatar: avatarPlaceholderUrl,
 				accountId,
 			}
 		);
@@ -84,5 +84,25 @@ export const verifySecret = async ({
 		return parseStringify({ sessionId: session.$id });
 	} catch (error) {
 		handleError(error, 'Failed to verify OTP');
+	}
+};
+
+export const getCurrentUser = async () => {
+	try {
+		const { databases, account } = await createSessionClient();
+
+		const result = await account.get();
+
+		const user = await databases.listDocuments(
+			appwriteConfig.databaseId,
+			appwriteConfig.usersCollectionId,
+			[Query.equal('accountId', result.$id)]
+		);
+
+		if (user.total <= 0) return null;
+
+		return parseStringify(user.documents[0]);
+	} catch (error) {
+		console.log(error);
 	}
 };
