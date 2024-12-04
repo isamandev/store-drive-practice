504300f1660439a7a4af079399846d699b3256a7
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-12-04 05:41:37 +0330

    Upload Files

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,27 +1,35 @@
 'use client';
-import React, { useState } from 'react';
+
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import { Separator } from '@/components/ui/separator';
 import Image from 'next/image';
+import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { Separator } from '@radix-ui/react-separator';
 import { navItems } from '@/constants';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { Button } from './ui/button';
-import FileUploader from './FileUploader';
+import { Button } from '@/components/ui/button';
+import FileUploader from '@/components/FileUploader';
 import { signOutUser } from '@/lib/actions/user.actions';
 
 interface Props {
-	ownerId: string;
+	$id: string;
 	accountId: string;
 	fullName: string;
 	avatar: string;
 	email: string;
 }
 
-const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props) => {
+const MobileNavigation = ({
+	$id: ownerId,
+	accountId,
+	fullName,
+	avatar,
+	email,
+}: Props) => {
 	const [open, setOpen] = useState(false);
 	const pathname = usePathname();
+
 	return (
 		<header className='mobile-header'>
 			<Image
@@ -31,6 +39,7 @@ const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props
 				height={52}
 				className='h-auto'
 			/>
+
 			<Sheet open={open} onOpenChange={setOpen}>
 				<SheetTrigger>
 					<Image src='/assets/icons/menu.svg' alt='Search' width={30} height={30} />
@@ -52,31 +61,32 @@ const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props
 						</div>
 						<Separator className='mb-4 bg-light-200/20' />
 					</SheetTitle>
+
 					<nav className='mobile-nav'>
 						<ul className='mobile-nav-list'>
-							{navItems.map(({ url, name, icon }) => {
-								return (
-									<Link key={name} href={url} className='lg:w-full'>
-										<li
-											className={cn('mobile-nav-item', pathname === url && 'shad-active')}
-										>
-											<Image
-												src={icon}
-												width={24}
-												height={24}
-												alt='icon'
-												className={cn('nav-icon', pathname === url && 'nav-icon-active')}
-											/>
-											<p>{name}</p>
-										</li>
-									</Link>
-								);
-							})}
+							{navItems.map(({ url, name, icon }) => (
+								<Link key={name} href={url} className='lg:w-full'>
+									<li
+										className={cn('mobile-nav-item', pathname === url && 'shad-active')}
+									>
+										<Image
+											src={icon}
+											alt={name}
+											width={24}
+											height={24}
+											className={cn('nav-icon', pathname === url && 'nav-icon-active')}
+										/>
+										<p>{name}</p>
+									</li>
+								</Link>
+							))}
 						</ul>
 					</nav>
+
 					<Separator className='my-5 bg-light-200/20' />
-					<div className='flex flex-col justify-between gap-5'>
-						<FileUploader />
+
+					<div className='flex flex-col justify-between gap-5 pb-5'>
+						<FileUploader ownerId={ownerId} accountId={accountId} />
 						<Button
 							type='submit'
 							className='mobile-sign-out-button'
