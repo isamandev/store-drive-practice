941b12f9d1f7350889bd8ad5083d3e2e644e9e45
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-12-06 13:05:44 +0330

    File Card

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '100MB',
+		},
+	},
 	images: {
 		remotePatterns: [
 			{ protocol: 'https', hostname: 'upload.wikimedia.org' },
