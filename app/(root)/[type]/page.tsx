abbc0d31128afa40c774655c979ade52549fe1f7
Saman Abbasi <samanabbasi.ir@gmail.com>
2024-12-13 10:46:37 +0330

    Filter Files & Global Search

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,30 +1,38 @@
-import Card from '@/components/Card';
+import React from 'react';
 import Sort from '@/components/Sort';
 import { getFiles } from '@/lib/actions/file.actions';
 import { Models } from 'node-appwrite';
-import React from 'react';
+import Card from '@/components/Card';
+import { getFileTypesParams } from '@/lib/utils';
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
 	const type = ((await params)?.type as string) || '';
-	const files = await getFiles();
+	const searchText = ((await searchParams)?.query as string) || '';
+	const sort = ((await searchParams)?.sort as string) || '';
+
+	const types = getFileTypesParams(type) as FileType[];
+
+	const files = await getFiles({ types, searchText, sort });
+
 	return (
 		<div className='page-container'>
-			<section className='h1 capitalize'>
-				<h1>{type}</h1>
+			<section className='w-full'>
+				<h1 className='h1 capitalize'>{type}</h1>
+
 				<div className='total-size-section'>
 					<p className='body-1'>
-						Total:
-						<span className='h-5'>
-							{/* {totalSize} */}
-							9MB
-						</span>
+						Total: <span className='h5'>0 MB</span>
 					</p>
+
 					<div className='sort-container'>
-						<p className='body-1 hidden sm:block text-light-200'>Sort by:</p>
+						<p className='body-1 hidden text-light-200 sm:block'>Sort by:</p>
+
 						<Sort />
 					</div>
 				</div>
 			</section>
+
+			{/* Render the files */}
 			{files.total > 0 ? (
 				<section className='file-list'>
 					{files.documents.map((file: Models.Document) => (
